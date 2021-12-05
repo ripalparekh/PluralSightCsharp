0d6b33c5db2dc75d3a3f80d7240c03c9d6a99cd7
ripal <ripal@LAPTOP-PJ1O34EU>
2021-12-05 22:19:54 +0000

    end chapter 2

---
+++ Course Maerials/c-sharp-big-picture/03/demos/xlang/app/Program.cs
@@ -0,0 +1,5 @@
+﻿var a = 30;
+var b = 12;
+var sum = fscalc.Calculator.Subtract(a, b);
+
+System.Console.WriteLine($"{a} + {b} = {sum}");
--- PluralSightFundamentals/01FirstProject/Program.cs
+++ PluralSightFundamentals/01FirstProject/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace BuildingBlocks;
+
+class BuildingBlocks
+{
+
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Hello, World!");
+    }
+}
